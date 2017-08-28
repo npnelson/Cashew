4eca201f4cfb11737febfdccd58234ced9c8216c
Nicholas P. Nelson <nnelson1@insight.rr.com>
2017-08-28 10:13:20 -0400

    add mvc/webapi samples

---
+++ samples/MVCTest/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using swagger;
+
+namespace MVCTest.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IMyAPI _api;
+
+        public HomeController(IMyAPI api)
+        {
+            _api = api;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var result = await _api.ApiValuesGetAsync();
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
---
+++ samples/MVCTest/Startup.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using swagger;
+using CacheManager.Core;
+using Cashew.Adapters.CacheManager;
+using Cashew.Keys;
+using Cashew;
+using Microsoft.Rest;
+
+namespace MVCTest
+{
+    public class Startup
+    {
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new Microsoft.Extensions.Configuration.ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                .AddEnvironmentVariables();
+            Configuration = builder.Build();
+        }
+
+        public IConfigurationRoot Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Add framework services.
+            services.AddMvc();
+           var cacheConfig = new CacheManager.Core.ConfigurationBuilder().WithMicrosoftMemoryCacheHandle().Build();
+           var cache = new BaseCacheManager<object>(cacheConfig);
+           var cacheAdapter = new CacheManagerAdapter(cache);
+            var keyStrategy = new HttpStandardKeyStrategy(cacheAdapter);
+            var handler = new HttpCachingHandler(cacheAdapter, keyStrategy);
+            var api = new MyAPI(handler);
+            api.BaseUri = new Uri("http://localhost:3670");
+            services.AddSingleton<IMyAPI>(api);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+            loggerFactory.AddDebug();
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseBrowserLink();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            app.UseStaticFiles();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
